2071e75ae50c88cee2a591e0de1e058f4eec26e8
ivanpugachtd@gmail.com <ivanpugachtd@gmail.com>
2024-09-23 15:53:16 +0300

    move vectorizer to TextProcessor to ensure consistent response

--- src/data_manager/text_preprocessor.py
+++ src/data_manager/text_preprocessor.py
@@ -10,21 +10,38 @@ class SentencesProcessor:
     def __init__(self, text: str, language: str = "english") -> None:
         self._text = text
         self._language = language
-        self._sentences: List[str] = []
-        
-    def init(self) -> Self:
+        self._sentences: List[str] | None = None
+
+    def _init(self) -> Self:
         self._sentences = sent_tokenize(self._text, self._language)
         print(f"Sentences type: {type(self._sentences)}")
         return self
-        
+
     def __getitem__(self, index: int) -> str:
+        if self._sentences is None:
+            self._init()
+        if not self._sentences:
+            raise ValueError("No sentences found")
+        if not 0 <= index < len(self._sentences):
+            raise IndexError("Index out of range")
+
         return self._sentences[index]
-        
+
+    def get_sentences(self) -> List[str]:
+        if self._sentences is None:
+            self._init()
+
+        if not self._sentences:
+            raise ValueError("No sentences found")
+
+        return self._sentences
+
 
 class TextPreprocessor:
-    def __init__(self) -> None:
+    def __init__(self, vectorizer: TfidfVectorizer) -> None:
         self.stop_words = set(stopwords.words("english"))
         self.punctuation = set(string.punctuation)
+        self.vectorizer = vectorizer
 
     def _preprocess_sentence(self, sentence: str) -> str:
         sentence = sentence.lower()
@@ -41,6 +58,9 @@ class TextPreprocessor:
             self._preprocess_sentence(sentence) for sentence in sentences
         )
         return preprocessed_sentences
-    
-    def vectorize(self, sentences: List[str], vectorizer: TfidfVectorizer) -> spmatrix:
-        return vectorizer.fit_transform(self.preprocess_text(sentences))
+
+    def vectorize(self, sentences: List[str]) -> spmatrix:
+        return self.vectorizer.fit_transform(self.preprocess_text(sentences))
+
+    def transform(self, question: str) -> spmatrix:
+        return self.vectorizer.transform([self._preprocess_sentence(question)])
